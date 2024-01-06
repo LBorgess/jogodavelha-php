d575c3f23a0945852a37fecc7b17626a4ba7df2b
LBorgess <lucas_rancer@hotmail.com>
2024-01-06 17:39:15 -0300

    Criação da página de erro

---
+++ app/404.php
@@ -0,0 +1,9 @@
+<?php defined('CONTROL') or die('Acesso negado.'); ?>
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-6 text-center alert alert-danger p-5">
+            <h4 class="mb-5">404 - Página não encontrada</h4>
+        </div>
+    </div>
+</div>
