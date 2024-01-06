163ba5991fb002670fd6c35bfebd375195c544f1
LBorgess <lucas_rancer@hotmail.com>
2024-01-06 19:05:13 -0300

    Arrumado CSS

--- includes/header.php
+++ includes/header.php
@@ -5,8 +5,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./includes/css/main.css">
     <link rel="stylesheet" href="./includes/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./includes/css/main.css">
     <title>Jogo da Velha</title>
 </head>
 
